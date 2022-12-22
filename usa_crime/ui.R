25830eafeed601e03bb206e6a3d2b26fc1dc1d1f
Ronak Rah <ronakrahmati25@gmail.com>
2022-12-22 08:22:05 +0100

    dashboardHeader is set

--- usa_crime/ui.R
+++ usa_crime/ui.R
@@ -3,7 +3,14 @@ library(shiny)
 library(shinydashboard)
 
 dashboardPage(
-  dashboardHeader(),
+  #-----------------------------|Dashboard Header|--------------------------
+  dashboardHeader(title="Exploring the USA  Crime data ", 
+                  titleWidth = 640, 
+                  tags$li(class="dropdown",
+                  tags$a(href="https://github.com/RonakRah/shiny_app", icon("github"),
+                         "My Github", target="_blank"))
+                  ),
+  #
   dashboardSidebar(),
   dashboardBody()
   
