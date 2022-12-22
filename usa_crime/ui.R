dd86d00739e66d854f4b5519ad93307b9a7a1f1f
Ronak Rah <ronakrahmati25@gmail.com>
2022-12-22 08:41:23 +0100

    dashboard sidebar is set

--- usa_crime/ui.R
+++ usa_crime/ui.R
@@ -10,8 +10,20 @@ dashboardPage(
                   tags$a(href="https://github.com/RonakRah/shiny_app", icon("github"),
                          "My Github", target="_blank"))
                   ),
-  #
-  dashboardSidebar(),
+  #------------------------------|Dashboard Sidebar|-------------------------
+  dashboardSidebar(
+    #Menu of dashboard
+    
+    sidebarMenu(
+      id="sidebar",
+      # 1- item in menu
+      menuItem("Dataset", tabname = "Dataset" , icon=icon("database")),
+      # 2- item in menu
+      menuItem(text = "Visualization", tabName = "Visualization", icon = icon("chart-line")),
+      # 3- item in menu
+      menuItem(text = "Color Theme Map", tabName = "Choropleth Map", icon = icon("map"))
+    )
+  ),
   dashboardBody()
   
   
