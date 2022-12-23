597fc5f3b464166cb1dd4209e623ea0c40367362
Ronak Rah <ronakrahmati25@gmail.com>
2022-12-23 14:28:03 +0100

    boxes for data item in menu is done

--- usa_crime/ui.R
+++ usa_crime/ui.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(shinydashboard)
 
+shinyUI(
 dashboardPage(
   #-----------------------------|Dashboard Header|--------------------------
   dashboardHeader(title="Exploring the USA  Crime data ", 
@@ -17,14 +18,32 @@ dashboardPage(
     sidebarMenu(
       id="sidebar",
       # 1- item in menu
-      menuItem("Dataset", tabname = "Dataset" , icon=icon("database")),
+      menuItem(text="Dataset", tabName = "dataset" , icon=icon("database")),
       # 2- item in menu
       menuItem(text = "Visualization", tabName = "Visualization", icon = icon("chart-line")),
       # 3- item in menu
-      menuItem(text = "Color Theme Map", tabName = "Choropleth Map", icon = icon("map"))
+      menuItem(text = "Color Theme Map", tabName = "Choropleth-Map", icon = icon("map"))
     )
+  
   ),
-  dashboardBody()
+  #-----------------------------|Dashboard Body|----------------------
+  dashboardBody(
+    tabItems(
+      # items for first menu tab
+      tabItem(tabName = "dataset",
+              #first tab box
+              tabBox(id="t1",width = 12,tabPanel("about",icon=icon("address-card")),
+              # second tab box
+              tabPanel("Data",icon = icon("table")),
+              tabPanel("Structure",icon=icon("uncharted")),
+              tabPanel("Summary Stats",icon=icon("chart-pie"))),
+              
+      
+      
+      
+      )
+   
   
   
+  )))
 )
