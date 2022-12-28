3cd482af331c4cee27b36246ddb0436affd46900
Ronak Rah <ronakrahmati25@gmail.com>
2022-12-28 14:55:57 +0100

    adding the conditional pannel for the input boxes done

--- usa_crime/ui.R
+++ usa_crime/ui.R
@@ -21,10 +21,10 @@ dashboardPage(
       menuItem(text="Dataset", tabName = "dataset" , icon=icon("database")),
       # 2- item in menu
       menuItem(text = "Visualization", tabName = "Visual", icon = icon("chart-line")),
-      selectInput(inputId ="var1" , label = "Choose a Crime to see the distribution", choices = crimes , selected = "Rape" ),#for histo and boxplot
+      conditionalPanel("input.sidebar == 'Visual' && input.t2 == 'dist'", selectInput(inputId = "var1" , label ="Select the Crime" , choices = crimes, selected = "Murder")),#for histo and boxplot
       
-      selectInput(inputId ="varA" , label = "Choose a Crime for X axis", choices = crimes , selected = "Assault" ), # fort scatter plot var A
-      selectInput(inputId ="varB" , label = "Choose a Crime for Y axis", choices = crimes , selected = "Murder" ), # fort scatter plot var B
+      conditionalPanel("input.sidebar == 'Visual' && input.t2 == 'rel' ", selectInput(inputId = "varA" , label ="Select the X axis" , choices = crimes, selected = "Rape")), # fort scatter plot var A
+      conditionalPanel("input.sidebar == 'Visual' && input.t2 == 'rel' ", selectInput(inputId = "varB" , label ="Select the Y axis" , choices = crimes, selected = "Assault")), # fort scatter plot var B
       # 3- item in menu
       menuItem(text = "Color Theme Map", tabName = "Choroplet-Map", icon = icon("map"))
     )
@@ -58,7 +58,7 @@ dashboardPage(
               #defining tab boxes
               tabBox(id="t2", width=14, 
               tabPanel(title ="Crime Trends in each State",value = "trends"),  # first tab box
-              tabPanel(title ="Distribution",plotlyOutput("histogramplot")), # second tab box
+              tabPanel(title ="Distribution",plotlyOutput("histogramplot"),value = "dist"), # second tab box
               tabPanel(title ="Correlation Matrix"), # third tab box
               tabPanel(title =" Arrest types & Urban Population Relationship",plotlyOutput("scatter"),value="rel",
                        radioButtons(inputId ="method" , label = "Select The method" , choices = c("loess", "lm"), selected = "loess" , inline = TRUE)), # fourth tab box
